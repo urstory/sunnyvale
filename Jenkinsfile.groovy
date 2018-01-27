e60615919bd1b4944430566120dafbc3b18a7476
kim sungpark <urstory@gmail.com>
2018-01-27 12:05:55 +0900

    fix Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -97,6 +97,23 @@ node('', {
             println "Git CheckOut Skip"
         }
     })
+
+    stage("build",{
+        if(useBuild){
+            withMaven(
+                    // Maven installation declared in the Jenkins "Global Tool Configuration"
+                    maven: 'M3',
+                    // Maven settings.xml file defined with the Jenkins Config File Provider Plugin
+                    // Maven settings and global settings can also be defined in Jenkins Global Tools Configuration
+                    mavenSettingsConfig: 'my-maven-settings',
+                    mavenLocalRepo: '.m2') {
+
+                // Run the maven build
+                sh "mvn package -Dmaven.test.skip=true"
+
+            }
+        }
+    })
 })
 
 node('', {
