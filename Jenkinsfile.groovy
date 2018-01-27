51467ee4903a368e6a32b18e482cc11b2af823be
kim sungpark <urstory@gmail.com>
2018-01-27 12:11:30 +0900

    fix Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -106,7 +106,7 @@ node('', {
                     // Maven settings.xml file defined with the Jenkins Config File Provider Plugin
                     // Maven settings and global settings can also be defined in Jenkins Global Tools Configuration
                     mavenSettingsConfig: 'my-maven-settings',
-                    mavenLocalRepo: '.m2') {
+                    mavenLocalRepo: '/opt/maven') {
 
                 // Run the maven build
                 sh "mvn package -Dmaven.test.skip=true"
